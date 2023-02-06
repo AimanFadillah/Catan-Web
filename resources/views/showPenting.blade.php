d6151f74f740f1fad251b0493906514af0faf11d
Aiman Fadillah <mlakece914@gmail.com>
2023-02-06 14:48:59 +0700

    new

--- resources/views/showPenting.blade.php
+++ resources/views/showPenting.blade.php
@@ -4,12 +4,25 @@
     
     <div class="container mt-10">
         <div class="row justify-content-center ">
-            <div class="col-md-10 rounded shadow " style="background-color: #FAF4B7">
-                <div class="d-flex justify-content-between align-items-center">
-                    <h3>{{ $Penting->judul }}</h3>
-                    <h6>{{ $Penting->created_at->format("t-m-Y") }}</h6>
+            <div class="col-md-10 shadow text-light" style="background-color: #20262E">
+                <div class="d-flex justify-content-between align-items-center py-2">
+                    <div class="">
+                        <i class="bi bi-fullscreen fs-4 ms-2"></i>
+                        <i class="bi bi-filetype-pdf ms-2 fs-4"></i>
+                    </div>
+                    <div class="">
+                        <span class="py-1 px-3 rounded bg-primary me-2"></span>
+                        <span class="py-1 px-3 rounded bg-info me-2"></span>
+                        <span class="py-1 px-3 rounded bg-success me-2"></span>
+                        <span class="py-1 px-3 rounded bg-danger me-2"></span>
+                        <span class="py-1 px-3 rounded bg-warning "></span>
+                    </div>
                 </div>
-            </div> 
+            </div>
+            <div class="col-md-10 rounded-bottom shadow" style="background-color: #F7F5EB" >
+                <h1 class="text-center mt-2" >{{ $Penting->judul }}</h1>
+                {!! $Penting->body !!}
+            </div>
         </div>
     </div>
 
