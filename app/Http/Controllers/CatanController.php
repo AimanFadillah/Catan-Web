f1e115fee9454308a7b5018684efa106b94c4906
Aiman Fadillah <mlakece914@gmail.com>
2023-02-11 13:13:12 +0700

    new

--- app/Http/Controllers/CatanController.php
+++ app/Http/Controllers/CatanController.php
@@ -2,16 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Mingguan;
+use App\Models\Penting;
+use App\Models\Sekilas;
 use Illuminate\Http\Request;
 
 class CatanController extends Controller
 {
     public function index () {
-        return view("home");
-    }
-
-    public function dataTable () {
-        return view("test.dataTable");
+        return view("home",[
+            "barisPenting" => Penting::where("user_id",auth()->user()->id)->count(),
+            "barisMingguan" => Mingguan::where("user_id",auth()->user()->id)->count(),
+            "barisSekilas" => Sekilas::where("user_id",auth()->user()->id)->count(),
+        ]);
     }
 
 }
